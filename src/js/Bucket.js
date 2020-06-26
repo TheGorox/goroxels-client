2c5857c3370bd26945f9a4b89fac63b5744e89e2
TheGorox <gnikita.010@gmail.com>
2020-06-26 03:14:33 +0300

    Added(stolen) Bucket

---
+++ src/js/Bucket.js
@@ -0,0 +1,38 @@
+class Bucket{
+    constructor(delay, max){
+        this._allowance = 0;
+
+        this.delay = delay;
+        this.max = max;
+
+        this.lastCheck = Date.now();
+    }
+
+    get allowance () {
+        this._allowance += (Date.now() - this.lastCheck) / this.delay;
+
+        this.lastCheck = Date.now();
+
+        if(this._allowance > this.max) this._allowance = this.max;
+
+        return this._allowance
+    }
+
+    set allowance(c){
+        this._allowance = c;
+    }
+
+    spend(count){
+        if(this.delay === 0) return true;
+
+        let allow = this.allowance;
+
+        if(allow < count) return false;
+
+        this.allowance  = allow - count;
+
+        return true
+    }
+}
+
+module.exports = Bucket
