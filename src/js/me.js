6ebf6a63e98e5a0ef39502a09ea84e2f4c6e681a
Gorox <gnikita.010@gmail.com>
2020-12-24 21:59:34 +0300

    Новые роли + прятать то, что смертные видеть не должны

--- src/js/me.js
+++ src/js/me.js
@@ -1,10 +1,23 @@
+import { ROLE } from './constants'
+
 export default {
     registered: false,
     name: null,
-    role: 'guest',
+    role: ROLE.USER,
     update(newMe){
         this.registered = newMe.registered || false;
         this.name = newMe.name || null;
-        this.role = newMe.role || 'guest';
+        this.role = +newMe.role || ROLE.USER;
+
+        switch(this.role){
+            case ROLE.USER:
+                $('.admin').hide();
+                $('.mod').hide();
+                break
+            case ROLE.ADMIN:
+                $('.admin').show();
+            case ROLE.MOD:
+                $('.mod').show();
+        }
     }
 }
