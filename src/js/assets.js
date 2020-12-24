77a2ebf3fae9b288b5a534e86ab73f874453d810
Gorox <gnikita.010@gmail.com>
2020-12-24 21:53:19 +0300

    Импорт стилей toastr + TODO

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,6 +1,8 @@
-import '../img/user.png'
+// TODO use CopyWebpackPlugin to clone static files, 
+// then replace imports with static paths
 
 import '../css/colorVars.css'
 import '../css/style.css'
+import '../../node_modules/toastr/build/toastr.css'
 
 export default {}
